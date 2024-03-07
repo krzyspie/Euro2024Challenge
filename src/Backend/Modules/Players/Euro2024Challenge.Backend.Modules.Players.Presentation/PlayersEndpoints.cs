b0e57cc5fd72f06631007aea6618f6db8a22796b
krzysiek <krzys_7@op.pl>
2024-03-07 21:36:53 +0100

    Add  presentation layer and players endpoint method

---
+++ src/Backend/Modules/Players/Euro2024Challenge.Backend.Modules.Players.Presentation/PlayersEndpoints.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.AspNetCore.Routing;
+
+namespace Euro2024Challenge.Backend.Modules.Players.Presentation
+{
+    public static class PlayersEndpoints
+    {
+        public static void MapPlayersEndpoints(this IEndpointRouteBuilder app)
+        {
+
+        }
+    }
+}
