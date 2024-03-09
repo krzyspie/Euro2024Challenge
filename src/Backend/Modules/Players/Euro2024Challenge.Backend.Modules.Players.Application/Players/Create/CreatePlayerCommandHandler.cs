6cb0c9d78dd2b741515bc42428c77121722de8d0
krzysiek <krzys_7@op.pl>
2024-03-09 15:31:54 +0100

    add players repository

--- src/Backend/Modules/Players/Euro2024Challenge.Backend.Modules.Players.Application/Players/Create/CreatePlayerCommandHandler.cs
+++ src/Backend/Modules/Players/Euro2024Challenge.Backend.Modules.Players.Application/Players/Create/CreatePlayerCommandHandler.cs
@@ -1,13 +1,22 @@
 ﻿using Euro2024Challenge.Backend.Modules.Players.Application.Players.Commands;
+using Euro2024Challenge.Backend.Modules.Players.Domain.Players.Repositories;
 using MediatR;
 
 namespace Euro2024Challenge.Backend.Modules.Players.Application.Players.Create
 {
     internal sealed class CreatePlayerCommandHandler : IRequestHandler<CreatePlayerCommand, Unit>
     {
-        public Task<Unit> Handle(CreatePlayerCommand request, CancellationToken cancellationToken)
+        private readonly IPlayersRepository _playersRepository;
+        public CreatePlayerCommandHandler(IPlayersRepository playersRepository)
         {
-            throw new NotImplementedException();
+            _playersRepository = playersRepository;
+        }
+
+        public async Task<Unit> Handle(CreatePlayerCommand request, CancellationToken cancellationToken)
+        {
+            await _playersRepository.Create(request.Email, request.Username);
+
+            return Unit.Value;
         }
     }
 }
---
+++ src/Backend/Modules/Players/Euro2024Challenge.Backend.Modules.Players.Domain/Players/Repositories/IPlayersRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Euro2024Challenge.Backend.Modules.Players.Domain.Players.Repositories
+{
+    public interface IPlayersRepository
+    {
+        Task Create(string email, string userName);
+    }
+}
