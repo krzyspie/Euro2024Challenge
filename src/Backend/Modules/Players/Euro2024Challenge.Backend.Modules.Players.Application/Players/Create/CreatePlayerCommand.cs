0a69a907ac9cbb3762e243e88e423559e00ccd43
krzysiek <krzys_7@op.pl>
2024-03-09 14:32:48 +0100

    Add create command and handler

---
+++ src/Backend/Modules/Players/Euro2024Challenge.Backend.Modules.Players.Application/Players/Create/CreatePlayerCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euro2024Challenge.Backend.Modules.Players.Application.Players.Commands
+{
+    internal sealed record CreatePlayerCommand(string Email, string Username) : IRequest<Unit>;
+}
---
+++ src/Backend/Modules/Players/Euro2024Challenge.Backend.Modules.Players.Application/Players/Create/CreatePlayerCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using Euro2024Challenge.Backend.Modules.Players.Application.Players.Commands;
+using MediatR;
+
+namespace Euro2024Challenge.Backend.Modules.Players.Application.Players.Create
+{
+    internal sealed class CreatePlayerCommandHandler : IRequestHandler<CreatePlayerCommand, Unit>
+    {
+        public Task<Unit> Handle(CreatePlayerCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
