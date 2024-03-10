adea3261eba8afef67d476b5e6db114ed2c53220
krzysiek <krzys_7@op.pl>
2024-03-10 21:07:38 +0100

    add create player request

---
+++ src/Backend/Modules/Players/Euro2024Challenge.Backend.Modules.Players.Application/Players/Create/CreatePlayerRequest.cs
@@ -0,0 +1,4 @@
+﻿namespace Euro2024Challenge.Backend.Modules.Players.Application.Players.Create
+{
+    public sealed record CreatePlayerRequest(string Email, string Username);
+}
--- src/Backend/Modules/Players/Euro2024Challenge.Backend.Modules.Players.Presentation/PlayersEndpoints.cs
+++ src/Backend/Modules/Players/Euro2024Challenge.Backend.Modules.Players.Presentation/PlayersEndpoints.cs
@@ -1,9 +1,10 @@
-﻿
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Routing;
-using System.Diagnostics;
+using MediatR;
+
+using Euro2024Challenge.Backend.Modules.Players.Application.Players.Create;
+using Euro2024Challenge.Backend.Modules.Players.Application.Players.Commands;
 
 namespace Euro2024Challenge.Backend.Modules.Players.Presentation
 {
@@ -13,8 +14,15 @@ namespace Euro2024Challenge.Backend.Modules.Players.Presentation
         {
             var players = app.MapGroup("players-module/players");
 
-            players.MapPost("", () => "Created")
+            players.MapPost("", () => CreatePlayer)
                 .Produces(201);
+        }
+
+        private static async Task<IResult> CreatePlayer(ISender sender, CreatePlayerRequest createPlayerRequest)
+        {
+            await sender.Send(new CreatePlayerCommand(createPlayerRequest.Email, createPlayerRequest.Username));
+
+            return Results.Ok();
         }
     }
 }
