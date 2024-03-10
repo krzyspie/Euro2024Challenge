bb2054580be9d8a8e78967defb963e31f638805b
krzysiek <krzys_7@op.pl>
2024-03-10 20:55:32 +0100

    module registration

--- src/Backend/Modules/Players/Euro2024Challenge.Backend.Modules.Players.Api/Program.cs
+++ src/Backend/Modules/Players/Euro2024Challenge.Backend.Modules.Players.Api/Program.cs
@@ -1,9 +1,18 @@
+using Euro2024Challenge.Backend.Modules.Players.Application;
+using Euro2024Challenge.Backend.Modules.Players.Infrastructure;
+using Euro2024Challenge.Backend.Modules.Players.Presentation;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services
+    .AddApplication()
+    .AddInfrastructure()
+    .AddPresentation();
 
 var app = builder.Build();
 
@@ -15,5 +24,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapPlayersEndpoints();
 
 app.Run();
