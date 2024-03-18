739da96d9ce70aa4c366d293fef867ba0587940f
krzysiek <krzys_7@op.pl>
2024-03-18 19:17:47 +0100

    Add entities to dbcontext. Add dbcontext to repository

--- src/Backend/Modules/Players/Euro2024Challenge.Backend.Modules.Players.Infrastructure/Database/PlayersDbContext.cs
+++ src/Backend/Modules/Players/Euro2024Challenge.Backend.Modules.Players.Infrastructure/Database/PlayersDbContext.cs
@@ -6,10 +6,17 @@ namespace Euro2024Challenge.Backend.Modules.Players.Infrastructure.Database
     internal class PlayersDbContext : DbContext
     {
         public DbSet<Player> Players { get; set; }
+        public DbSet<MatchBet> MatchesMets { get; set; }
+        public DbSet<TopScorerBet> TopScorerBets { get; set; }
+        public DbSet<TurnamentWinnerBet> TurnamentWinnerBets { get; set; }
 
         public PlayersDbContext(DbContextOptions<PlayersDbContext> options) : base(options)
+        {                
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-                
+            modelBuilder.HasDefaultSchema("players");
         }
     }
 }
--- src/Backend/Modules/Players/Euro2024Challenge.Backend.Modules.Players.Infrastructure/Database/Repositories/PlayersRepository.cs
+++ src/Backend/Modules/Players/Euro2024Challenge.Backend.Modules.Players.Infrastructure/Database/Repositories/PlayersRepository.cs
@@ -1,10 +1,20 @@
 ﻿using Euro2024Challenge.Backend.Modules.Players.Domain.Entities;
 using Euro2024Challenge.Backend.Modules.Players.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace Euro2024Challenge.Backend.Modules.Players.Infrastructure.Database.Repositories
 {
     internal class PlayersRepository : IPlayersRepository
     {
+        private readonly PlayersDbContext _context;
+        private readonly DbSet<Player> _players;
+
+        public PlayersRepository(PlayersDbContext context)
+        {
+            _context = context;
+            _players = _context.Players;
+        }
+
         public Task Create(Player player)
         {
             throw new NotImplementedException();
