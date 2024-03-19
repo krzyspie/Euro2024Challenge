d0c12ec6abc029c6692292bf2192d28ca4cc360d
krzysiek <krzys_7@op.pl>
2024-03-19 21:41:00 +0100

    Add player bets endpoint definition

---
+++ src/Backend/Modules/Players/Euro2024Challenge.Backend.Modules.Players.Presentation/PlayerBetsEndpoints.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+
+namespace Euro2024Challenge.Backend.Modules.Players.Presentation
+{
+    public static class PlayerBetsEndpoints
+    {
+        public static void MapPlayerBetsEndpoints(this IEndpointRouteBuilder app)
+        {
+            var playerBets = app.MapGroup("players-module/player-bets");
+        }
+    }
+}
