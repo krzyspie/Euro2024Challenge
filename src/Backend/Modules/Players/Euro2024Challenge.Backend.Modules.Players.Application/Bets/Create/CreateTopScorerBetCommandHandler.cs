e9084f5066dcb6fd9082754128cbc3a53808a3d4
krzysiek <krzys_7@op.pl>
2024-03-21 19:52:35 +0100

    Add top scorer bet command

---
+++ src/Backend/Modules/Players/Euro2024Challenge.Backend.Modules.Players.Application/Bets/Create/CreateTopScorerBetCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace Euro2024Challenge.Backend.Modules.Players.Application.Bets.Create
+{
+    public class CreateTopScorerBetCommandHandler : IRequestHandler<CreateTopScorerBetCommand, Unit>
+    {
+        public Task<Unit> Handle(CreateTopScorerBetCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
