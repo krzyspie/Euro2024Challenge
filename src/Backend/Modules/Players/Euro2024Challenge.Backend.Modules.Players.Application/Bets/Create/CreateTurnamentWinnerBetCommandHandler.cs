a4bcc642c5ded7d0f78f0d303e40ad2cb8e4c085
krzysiek <krzys_7@op.pl>
2024-03-22 15:31:23 +0100

    add create turnament winner bet command

---
+++ src/Backend/Modules/Players/Euro2024Challenge.Backend.Modules.Players.Application/Bets/Create/CreateTurnamentWinnerBetCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace Euro2024Challenge.Backend.Modules.Players.Application.Bets.Create
+{
+    public class CreateTurnamentWinnerBetCommandHandler : IRequestHandler<CreateTurnamentWinnerBetCommand, Unit>
+    {
+        public Task<Unit> Handle(CreateTurnamentWinnerBetCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
