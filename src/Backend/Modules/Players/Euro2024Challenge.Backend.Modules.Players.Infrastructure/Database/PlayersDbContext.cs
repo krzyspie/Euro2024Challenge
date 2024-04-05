ab82c7675bfe6b06dd782b1f80ea080ef197b46d
krzpie <krzys_7@op.pl>
2024-04-05 15:26:01 +0200

    Apply BaseDbContext to Players db context

--- src/Backend/Modules/Players/Euro2024Challenge.Backend.Modules.Players.Infrastructure/Database/PlayersDbContext.cs
+++ src/Backend/Modules/Players/Euro2024Challenge.Backend.Modules.Players.Infrastructure/Database/PlayersDbContext.cs
@@ -1,18 +1,15 @@
 ﻿using Euro2024Challenge.Backend.Modules.Players.Domain.Entities;
+using Euro2024Challenge.Shared.Database;
 using Microsoft.EntityFrameworkCore;
 
 namespace Euro2024Challenge.Backend.Modules.Players.Infrastructure.Database
 {
-    internal class PlayersDbContext : DbContext
+    internal class PlayersDbContext : BaseDbContext
     {
         public DbSet<Player> Players { get; set; }
         public DbSet<MatchBet> MatchMets { get; set; }
         public DbSet<TopScorerBet> TopScorerBets { get; set; }
         public DbSet<TurnamentWinnerBet> TurnamentWinnerBets { get; set; }
-
-        public PlayersDbContext(DbContextOptions<PlayersDbContext> options) : base(options)
-        {                
-        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
