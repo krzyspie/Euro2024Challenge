7e4a2160c8f3cf3239163970e4d08df4f72d8cc6
krzpie <krzys_7@op.pl>
2024-04-07 13:13:01 +0200

    Cleanup do context resistration

--- src/Backend/Shared/Euro2024Challenge.Shared/Database/Extensions.cs
+++ src/Backend/Shared/Euro2024Challenge.Shared/Database/Extensions.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 
@@ -10,27 +9,17 @@ namespace Euro2024Challenge.Shared.Database
         public static IServiceCollection AddPostgres<T>(this IServiceCollection services) where T : DbContext
         {
             services.ConfigureOptions<DatabaseOptionsSetup>();
-            // using var serviceProvider = services.BuildServiceProvider();
-            //var databaseOptions = serviceProvider.GetService<IOptions<DatabaseOptions>>()!.Value;
-            
-             services.AddDbContext<T>((serviceProvider, option) =>
-             {
+
+            services.AddDbContext<T>((serviceProvider, option) => 
+            {
                  var databaseOptions = serviceProvider.GetService<IOptions<DatabaseOptions>>()!.Value;
                  option.UseNpgsql(databaseOptions.ConnectionString, sqlOption =>
                  {
                      sqlOption.EnableRetryOnFailure(databaseOptions.MaxRetryCount);
                      sqlOption.CommandTimeout(databaseOptions.CommandTimeout);
-            
                  });
-             });
-
-            //using var serviceProvider = services.BuildServiceProvider();
-            // var configuration = serviceProvider.GetRequiredService<IConfiguration>();
-            // var connectionString = configuration.GetConnectionString("DbConnection");
-            //
-            // services.AddDbContext<T>(options =>
-            //     options.UseNpgsql(connectionString));
-
+            });
+            
             return services;
         }
     }
