126710bac7450890c3319f8a6bbd5ae562f3fe50
krzpie <krzys_7@op.pl>
2024-04-13 21:19:35 +0200

    Add responses dtos

--- src/Backend/Modules/Tournaments/Euro2024Challenge.Backend.Modules.Tournaments.Core/DTO/MatchResponse.cs
+++ src/Backend/Modules/Tournaments/Euro2024Challenge.Backend.Modules.Tournaments.Core/DTO/MatchResponse.cs
@@ -1,3 +1,11 @@
 namespace Euro2024Challenge.Backend.Modules.Tournaments.Core.DTO;
 
-public record MatchResponse(int Id, int Number, int GuestTeamId, int AwayTeamId, int GuestTeamGoals, int AwayTeamGoals, DateTime StartHour);
+public record MatchResponse(
+    int Id,
+    int Number,
+    int GuestTeamId,
+    int AwayTeamId,
+    int GuestTeamGoals,
+    int AwayTeamGoals,
+    DateTime StartHour);
+    
---
+++ src/Backend/Modules/Tournaments/Euro2024Challenge.Backend.Modules.Tournaments.Core/DTO/TeamResponse.cs
@@ -0,0 +1,3 @@
+namespace Euro2024Challenge.Backend.Modules.Tournaments.Core.DTO;
+
+public sealed record TeamResponse(int Id, string Name);
