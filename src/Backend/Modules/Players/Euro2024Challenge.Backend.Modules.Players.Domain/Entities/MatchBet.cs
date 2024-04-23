05ed7d9d7bf00bc65ddbd5a6bddfe0e6da72c47c
krzpie <krzys_7@op.pl>
2024-04-23 15:05:22 +0200

    Use MatchResult value object in MatchBet entity

--- src/Backend/Modules/Players/Euro2024Challenge.Backend.Modules.Players.Domain/Entities/MatchBet.cs
+++ src/Backend/Modules/Players/Euro2024Challenge.Backend.Modules.Players.Domain/Entities/MatchBet.cs
@@ -1,10 +1,12 @@
-﻿namespace Euro2024Challenge.Backend.Modules.Players.Domain.Entities
+﻿using Euro2024Challenge.Backend.Modules.Players.Domain.ValueObjects;
+
+namespace Euro2024Challenge.Backend.Modules.Players.Domain.Entities
 {
     public class MatchBet : BaseEntity
     {
         public Guid PlayerId { get; set; }
         public int MatchId { get; set; }
-        public int Result { get; set; }
+        public MatchResult Result { get; set; }
         public int HomeTeamGoals { get; set; }
         public int AwayTeamGoals { get; set; }
     }
