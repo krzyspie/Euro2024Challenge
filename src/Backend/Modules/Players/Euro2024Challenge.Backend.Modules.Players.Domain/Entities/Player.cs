d3a9fdfee1fec6b63f6e869ff0c88455629ca2d6
krzpie <krzys_7@op.pl>
2024-04-28 11:31:58 +0200

    Update Player entity

--- src/Backend/Modules/Players/Euro2024Challenge.Backend.Modules.Players.Domain/Entities/Player.cs
+++ src/Backend/Modules/Players/Euro2024Challenge.Backend.Modules.Players.Domain/Entities/Player.cs
@@ -4,5 +4,9 @@
     {
         public required string Email { get; set; }
         public required string Username { get; set; }
+
+        public TopScorerBet TopScorerBet { get; set; }
+        public TournamentWinnerBet TournamentWinnerBet { get; set; }
+        public ICollection<MatchBet> MatchBets { get; set; }
     }
 }
