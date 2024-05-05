db5c3a842865944a3c80170d602a94a936b801ab
krzpie <krzys_7@op.pl>
2024-05-05 18:34:37 +0200

    Migration

---
+++ src/Backend/Modules/Players/Euro2024Challenge.Backend.Modules.Players.Infrastructure/Database/Migrations/20240504104836_UpdateMatchBet.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Euro2024Challenge.Backend.Modules.Players.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateMatchBet : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Result",
+                schema: "players",
+                table: "MatchBets",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Winner",
+                schema: "players",
+                table: "MatchBets",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Winner",
+                schema: "players",
+                table: "MatchBets");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Result",
+                schema: "players",
+                table: "MatchBets",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
