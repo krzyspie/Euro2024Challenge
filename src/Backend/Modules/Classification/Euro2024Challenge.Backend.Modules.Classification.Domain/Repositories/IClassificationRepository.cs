9970f856924b24cdcb01ab0067678aaf29654922
krzpie <krzys_7@op.pl>
2024-05-09 21:13:15 +0200

    Add classification repository interface

---
+++ src/Backend/Modules/Classification/Euro2024Challenge.Backend.Modules.Classification.Domain/Repositories/IClassificationRepository.cs
@@ -0,0 +1,8 @@
+using Euro2024Challenge.Backend.Modules.Classification.Domain.Entities;
+
+namespace Euro2024Challenge.Backend.Modules.Classification.Domain.Repositories;
+
+public interface IClassificationRepository
+{
+    Task<IEnumerable<PlayerPoints>> GetAll();
+}
