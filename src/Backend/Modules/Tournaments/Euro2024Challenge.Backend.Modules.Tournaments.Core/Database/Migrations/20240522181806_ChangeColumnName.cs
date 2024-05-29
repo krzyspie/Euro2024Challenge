e968ba83ca67fbdd30f326b8ce5394ad4a314068
krzpie <krzys_7@op.pl>
2024-05-29 20:55:13 +0200

    change column name migration

---
+++ src/Backend/Modules/Tournaments/Euro2024Challenge.Backend.Modules.Tournaments.Core/Database/Migrations/20240522181806_ChangeColumnName.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Euro2024Challenge.Backend.Modules.Tournaments.Core.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeColumnName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Golas",
+                schema: "tournaments",
+                table: "Footballers",
+                newName: "Goals");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Goals",
+                schema: "tournaments",
+                table: "Footballers",
+                newName: "Golas");
+        }
+    }
+}
