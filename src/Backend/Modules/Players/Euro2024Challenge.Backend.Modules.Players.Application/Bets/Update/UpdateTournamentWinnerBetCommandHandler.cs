bca56f73e9c9c73166a141df06d6cfc08f3d0aa7
krzpie <krzys_7@op.pl>
2024-06-02 17:06:10 +0200

    update tournament winner bet implementation

---
+++ src/Backend/Modules/Players/Euro2024Challenge.Backend.Modules.Players.Application/Bets/Update/UpdateTournamentWinnerBetCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using Euro2024Challenge.Backend.Modules.Players.Domain.Entities;
+using Euro2024Challenge.Backend.Modules.Players.Domain.Repositories;
+using MediatR;
+
+namespace Euro2024Challenge.Backend.Modules.Players.Application.Bets.Update
+{
+    public class UpdateTournamentWinnerBetCommandHandler : IRequestHandler<UpdateTournamentWinnerBetCommand, Unit>
+    {
+        private readonly IPlayersBetsRepository _playersBetsRepository;
+
+        public UpdateTournamentWinnerBetCommandHandler(IPlayersBetsRepository playersBetsRepository)
+        {
+            _playersBetsRepository = playersBetsRepository;
+        }
+
+        public async Task<Unit> Handle(UpdateTournamentWinnerBetCommand request, CancellationToken cancellationToken)
+        {
+            var bet = new TournamentWinnerBet
+            {
+                TeamId = request.TeamId,
+                PlayerId = request.PLayerId
+            };
+
+            await _playersBetsRepository.UpdateTournamentWinnerBetAsync(bet);
+            
+            return Unit.Value;
+        }
+    }
+}
---
+++ src/Backend/Modules/Players/Euro2024Challenge.Backend.Modules.Players.Application/Bets/Update/UpdateTournamentWinnerBetRequest.cs
@@ -0,0 +1,4 @@
+﻿namespace Euro2024Challenge.Backend.Modules.Players.Application.Bets.Update
+{
+    public sealed record UpdateTournamentWinnerBetRequest(Guid PlayerId, int TeamId);
+}
