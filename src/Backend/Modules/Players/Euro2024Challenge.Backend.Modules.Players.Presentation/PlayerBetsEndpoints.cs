789ee8d671333a2cd1dacfa9eb8753d6915903c5
krzpie <krzys_7@op.pl>
2024-06-03 20:31:43 +0200

    Update bets endpoint methods

--- src/Backend/Modules/Players/Euro2024Challenge.Backend.Modules.Players.Presentation/PlayerBetsEndpoints.cs
+++ src/Backend/Modules/Players/Euro2024Challenge.Backend.Modules.Players.Presentation/PlayerBetsEndpoints.cs
@@ -1,5 +1,6 @@
 ﻿using Euro2024Challenge.Backend.Modules.Players.Application.Bets.Create;
 using Euro2024Challenge.Backend.Modules.Players.Application.Bets.Get;
+using Euro2024Challenge.Backend.Modules.Players.Application.Bets.Update;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
@@ -24,6 +25,15 @@ namespace Euro2024Challenge.Backend.Modules.Players.Presentation
                 .Produces(201);
 
             playerBets.MapPost("/tournament-winner", CreateTournamentWinnerBet)
+                .Produces(201);
+            
+            playerBets.MapPut("/match-bet", UpdateMatchBet)
+                .Produces(201);
+
+            playerBets.MapPut("/top-scorer", UpdateTopScorerBet)
+                .Produces(201);
+
+            playerBets.MapPut("/tournament-winner", UpdateTournamentWinnerBet)
                 .Produces(201);
 
 
@@ -50,6 +60,26 @@ namespace Euro2024Challenge.Backend.Modules.Players.Presentation
             return Results.Ok();
         }
 
+        private static async Task<IResult> UpdateMatchBet([FromServices] ISender sender, UpdateMatchBetRequest request)
+        {
+            await sender.Send(new UpdateMatchBetCommand(request.PlayerId, request.MatchId, request.Winner, request.HomeTeamGoals, request.AwayTeamGoals));
+
+            return Results.Ok();
+        }
+
+        private static async Task<IResult> UpdateTopScorerBet([FromServices] ISender sender, UpdateTopScorerBetRequest request)
+        {
+            await sender.Send(new UpdateTopScorerBetCommand(request.PlayerId, request.FootballerId, request.Goals));
+
+            return Results.Ok();
+        }
+
+        private static async Task<IResult> UpdateTournamentWinnerBet([FromServices] ISender sender, UpdateTournamentWinnerBetRequest request)
+        {
+            await sender.Send(new UpdateTournamentWinnerBetCommand(request.PlayerId, request.TeamId));
+
+            return Results.Ok();
+        }
         private static async Task<IResult> GetPlayerBets([FromServices] ISender sender, Guid playerId)
         {
             await sender.Send(new GetPlayerBetsQuery(playerId));
