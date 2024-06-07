59eb8e2770fff1373230452c49df839bd57cd7cb
krzpie <krzys_7@op.pl>
2024-06-07 11:08:24 +0200

    add tournament module api class

---
+++ src/Backend/Modules/Tournaments/Euro2024Challenge.Backend.Modules.Tournaments.Core/Clients/TournamentModuleApi.cs
@@ -0,0 +1,21 @@
+﻿using Euro2024Challenge.Backend.Modules.Tournament.Shared;
+
+namespace Euro2024Challenge.Backend.Modules.Tournaments.Core;
+
+public class TournamentModuleApi : ITournamentModuleApi
+{
+    public Task GetFootballer(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task GetMatches(int[] matchIds)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task GetTeam(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
