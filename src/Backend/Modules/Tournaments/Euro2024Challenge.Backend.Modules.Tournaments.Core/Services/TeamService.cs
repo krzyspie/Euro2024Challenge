16e0a1c5548fcf60beae33cb32d0822824b390d3
krzpie <krzys_7@op.pl>
2024-06-12 21:41:35 +0200

    Get team from cache

--- src/Backend/Modules/Tournaments/Euro2024Challenge.Backend.Modules.Tournaments.Core/Services/TeamService.cs
+++ src/Backend/Modules/Tournaments/Euro2024Challenge.Backend.Modules.Tournaments.Core/Services/TeamService.cs
@@ -1,29 +1,42 @@
 using Euro2024Challenge.Backend.Modules.Tournament.Shared.DTO;
 using Euro2024Challenge.Backend.Modules.Tournaments.Core.Extensions;
 using Euro2024Challenge.Backend.Modules.Tournaments.Core.Repositories;
+using Microsoft.EntityFrameworkCore.Storage.Json;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Euro2024Challenge.Backend.Modules.Tournaments.Core.Services;
 
 public class TeamService : ITeamService
 {
     private readonly ITeamRepository _teamRepository;
+    private readonly IMemoryCache _cache;
 
-    public TeamService(ITeamRepository teamRepository)
+    public TeamService(ITeamRepository teamRepository, IMemoryCache cache)
     {
         _teamRepository = teamRepository;
+        _cache = cache;
     }
     
     public async Task<IEnumerable<TeamResponse>> GetTeamsAsync(List<int> ids)
     {
+        var stats = _cache.GetCurrentStatistics();
+
         var teams = await _teamRepository.GetTeamsAsync(ids);
 
         return teams.ToTeamsResponse();
     }
 
-    public async Task<TeamResponse> GetTeamAsync(int ids)
+    public async Task<TeamResponse> GetTeamAsync(int id)
     {
-        var team = await _teamRepository.GetTeamAsync(ids);
-
-        return team.ToTeamResponse();
+        if(!_cache.TryGetValue(id, out string? teamName))
+        {
+            var team = await _teamRepository.GetTeamAsync(id);
+            
+            _cache.Set(id, team.Name);
+            
+            return team.ToTeamResponse();
+        }
+        
+        return new TeamResponse(id, teamName!);        
     }
 }
