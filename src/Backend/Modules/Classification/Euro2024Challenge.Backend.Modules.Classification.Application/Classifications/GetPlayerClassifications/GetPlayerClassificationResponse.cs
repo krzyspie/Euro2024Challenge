b883bdd7c36ce543760cd996bc5c981a1a688ad5
krzpie <krzys_7@op.pl>
2024-06-14 20:38:50 +0200

    Add request and response classes for get player classification

---
+++ src/Backend/Modules/Classification/Euro2024Challenge.Backend.Modules.Classification.Application/Classifications/GetPlayerClassifications/GetPlayerClassificationResponse.cs
@@ -0,0 +1,10 @@
+﻿using Euro2024Challenge.Backend.Modules.Classification.Domain.Entities;
+
+namespace Euro2024Challenge.Backend.Modules.Classification.Application;
+
+public class GetPlayerClassificationResponse
+{
+    public Guid PlayerId { get; set; }
+
+    public IReadOnlyCollection<PlayerPoints> BetsPoints { get; set; }
+}
