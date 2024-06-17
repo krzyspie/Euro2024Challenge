be0e46058aea7a52c808cc184e37eea3ef53fe52
krzpie <krzys_7@op.pl>
2024-06-17 20:58:00 +0200

    add extension class

---
+++ src/Backend/Modules/Classification/Euro2024Challenge.Backend.Modules.Classification.Application/Extensions/BetPointsExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Euro2024Challenge.Backend.Modules.Classification.Domain.Entities;
+
+namespace Euro2024Challenge.Backend.Modules.Classification.Application;
+
+public static class BetPointsExtensions
+{
+    public static GetPlayerClassificationsResponse ToGetPlayerClassificationsResponse(this BetPoints betPoints)
+    {
+        return null;
+    }
+
+}
