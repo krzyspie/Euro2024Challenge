4ca9f8902d39eb2345fd0310ec16221c184d2304
krzpie <krzys_7@op.pl>
2024-06-24 20:28:40 +0200

    InMemoryMessageQueue implementation

---
+++ src/Backend/Shared/Euro2024Challenge.Shared/Events/InMemoryMessageQueue.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Channels;
+
+namespace Euro2024Challenge.Shared;
+
+public class InMemoryMessageQueue
+{
+    private readonly Channel<IEvent> _channel =
+        Channel.CreateUnbounded<IEvent>();
+
+    public ChannelReader<IEvent> Reader => _channel.Reader;
+
+    public ChannelWriter<IEvent> Writer => _channel.Writer;
+}
