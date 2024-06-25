19439d255e7b15c7df9031c163211a028ae4748b
krzpie <krzys_7@op.pl>
2024-06-25 14:31:04 +0200

    Event bus implementation

---
+++ src/Backend/Shared/Euro2024Challenge.Shared/Events/EventBus.cs
@@ -0,0 +1,15 @@
+﻿namespace Euro2024Challenge.Shared;
+
+public class EventBus : IEventBus
+{
+    private readonly InMemoryMessageQueue _messageQueue;
+
+    public EventBus(InMemoryMessageQueue messageQueue)
+    {
+        _messageQueue = messageQueue;
+    }
+    public async Task PublishAsync<T>(T integrationEvent, CancellationToken cancellationToken) where T : class, IEvent
+    {
+        await _messageQueue.Writer.WriteAsync(integrationEvent, cancellationToken);
+    }
+}
