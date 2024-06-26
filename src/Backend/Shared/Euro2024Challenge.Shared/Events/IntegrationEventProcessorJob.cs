f5f786748c5fe44511a119132513e6073c8d001b
krzpie <krzys_7@op.pl>
2024-06-26 20:57:31 +0200

    Add background service

---
+++ src/Backend/Shared/Euro2024Challenge.Shared/Events/IntegrationEventProcessorJob.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace Euro2024Challenge.Shared;
+
+internal sealed class IntegrationEventProcessorJob : BackgroundService
+{
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        throw new NotImplementedException();
+    }
+}
