39e3bfeab868acbcf06b0063e52616c123346406
krzpie <krzys_7@op.pl>
2024-06-27 15:33:15 +0200

    event job implementation

---
+++ src/Backend/Shared/Euro2024Challenge.Shared/Events/IEventDispatcher.cs
@@ -0,0 +1,7 @@
+﻿namespace Euro2024Challenge.Shared;
+
+public interface IEventDispatcher
+{
+    Task PublishAsync<TEvent>(TEvent integrationEvent, CancellationToken cancellationToken = default)
+        where TEvent : class, IEvent;
+}
--- src/Backend/Shared/Euro2024Challenge.Shared/Events/IntegrationEventProcessorJob.cs
+++ src/Backend/Shared/Euro2024Challenge.Shared/Events/IntegrationEventProcessorJob.cs
@@ -4,8 +4,26 @@ namespace Euro2024Challenge.Shared;
 
 internal sealed class IntegrationEventProcessorJob : BackgroundService
 {
-    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    private readonly InMemoryMessageQueue _messageQueue;
+    private readonly IEventDispatcher _eventDispatcher;
+
+    public IntegrationEventProcessorJob(InMemoryMessageQueue messageQueue, IEventDispatcher eventDispatcher)
     {
-        throw new NotImplementedException();
+        _messageQueue = messageQueue;
+        _eventDispatcher = eventDispatcher;
+    }
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        await foreach (var integrationEvent in _messageQueue.Reader.ReadAllAsync(stoppingToken))
+        {
+            try
+            {
+                await _eventDispatcher.PublishAsync(integrationEvent, stoppingToken);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
     }
 }
