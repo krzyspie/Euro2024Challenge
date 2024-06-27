2857f076ed38ed2d26365c1880748cee4916856c
krzpie <krzys_7@op.pl>
2024-06-27 15:26:59 +0200

    event handler interface

---
+++ src/Backend/Shared/Euro2024Challenge.Shared/Events/IEventHandler.cs
@@ -0,0 +1,6 @@
+﻿namespace Euro2024Challenge.Shared;
+
+public interface IEventHandler<in TEvent> where TEvent : class, IEvent
+{
+    Task HandleAsync(TEvent integrationEvent, CancellationToken cancellationToken = default);
+}
