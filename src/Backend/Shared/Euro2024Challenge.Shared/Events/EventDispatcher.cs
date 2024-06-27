3d5f9bb4ec9aa68c60a3eeaeb196a984306af239
krzpie <krzys_7@op.pl>
2024-06-27 15:44:57 +0200

    event dispatcher implementation

---
+++ src/Backend/Shared/Euro2024Challenge.Shared/Events/EventDispatcher.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Euro2024Challenge.Shared;
+
+public class EventDispatcher : IEventDispatcher
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public EventDispatcher(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public async Task PublishAsync<TEvent>(TEvent integrationEvent, CancellationToken cancellationToken) where TEvent : class, IEvent
+    {
+        using var scope = _serviceProvider.CreateScope();
+            var handlers = scope.ServiceProvider.GetServices<IEventHandler<TEvent>>();
+
+            var tasks = handlers.Select(x => x.HandleAsync(integrationEvent));
+            await Task.WhenAll(tasks);
+    }
+}
