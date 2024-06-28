a4cfdbbd83d84590f7e65dfcca697d5d6b3e7c3a
krzpie <krzys_7@op.pl>
2024-06-28 21:34:01 +0200

    add integration event

---
+++ src/Backend/Modules/Tournaments/Euro2024Challenge.Backend.Modules.Tournaments.Core/IntegrationEvents/MatchUpdated.cs
@@ -0,0 +1,5 @@
+﻿using Euro2024Challenge.Shared;
+
+namespace Euro2024Challenge.Backend.Modules.Tournaments.Core;
+
+public record MatchUpdated(int MatchId, int HomeTeamGoals, int AwayTeamGoals) : IEvent;
