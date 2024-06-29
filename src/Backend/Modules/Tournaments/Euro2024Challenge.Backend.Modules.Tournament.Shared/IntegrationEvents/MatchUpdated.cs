dbf42b822a30460b88fb811e0edb82363e06fe13
krzpie <krzys_7@op.pl>
2024-06-29 09:27:48 +0200

    change integration event location

---
+++ src/Backend/Modules/Tournaments/Euro2024Challenge.Backend.Modules.Tournament.Shared/IntegrationEvents/MatchUpdated.cs
@@ -0,0 +1,5 @@
+﻿using Euro2024Challenge.Shared;
+
+namespace Euro2024Challenge.Backend.Modules.Tournament.Shared;
+
+public record class MatchUpdated(int MatchId, int HomeTeamGoals, int AwayTeamGoals) : IEvent;
--- src/Backend/Modules/Tournaments/Euro2024Challenge.Backend.Modules.Tournaments.Core/IntegrationEvents/MatchUpdated.cs
+++
@@ -1,5 +0,0 @@
-﻿using Euro2024Challenge.Shared;
-
-namespace Euro2024Challenge.Backend.Modules.Tournaments.Core;
-
-public record MatchUpdated(int MatchId, int HomeTeamGoals, int AwayTeamGoals) : IEvent;
