da0f79dab96ef17a46c54ffb45248ca17a38bf7e
krzpie <krzys_7@op.pl>
2024-07-01 20:56:18 +0200

    services registration

--- src/Backend/Shared/Euro2024Challenge.Shared/DependencyInjection.cs
+++ src/Backend/Shared/Euro2024Challenge.Shared/DependencyInjection.cs
@@ -6,6 +6,9 @@ public static class DependencyInjection
 {
     public static IServiceCollection RegisterShared(this IServiceCollection services)
     {
+        services.AddSingleton<IEventDispatcher, EventDispatcher>();
+        services.AddHostedService<IntegrationEventProcessorJob>();
+
         return services;
 
     }
