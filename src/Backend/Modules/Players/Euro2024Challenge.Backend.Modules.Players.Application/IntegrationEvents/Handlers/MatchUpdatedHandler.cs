fcb089c45297df495acc83d96bac3295beeb0ed4
krzpie <krzys_7@op.pl>
2024-07-06 20:41:33 +0200

    add integration event handler

---
+++ src/Backend/Modules/Players/Euro2024Challenge.Backend.Modules.Players.Application/IntegrationEvents/Handlers/MatchUpdatedHandler.cs
@@ -0,0 +1,14 @@
+﻿using Euro2024Challenge.Backend.Modules.Tournament.Shared;
+using Euro2024Challenge.Shared;
+
+namespace Euro2024Challenge.Backend.Modules.Players.Application;
+
+public class MatchUpdatedHandler : IEventHandler<MatchUpdated>
+{
+    public Task HandleAsync(MatchUpdated integrationEvent, CancellationToken cancellationToken = default)
+    {
+        Console.WriteLine("MatchUpdatedHandler");
+
+        return Task.CompletedTask;
+    }
+}
