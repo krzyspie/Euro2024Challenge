341f6d1ee5a943c812fb2d5fc36673634eb08d11
krzpie <krzys_7@op.pl>
2024-07-11 23:16:20 +0200

    add handler

---
+++ src/Backend/Modules/Classification/Euro2024Challenge.Backend.Modules.Classification.Application/IntegrationEvents/Handlers/PlayersMatchBetsClaculatedHandler.cs
@@ -0,0 +1,14 @@
+using Euro2024Challenge.Backend.Modules.Players.Shared;
+using Euro2024Challenge.Shared;
+
+namespace Euro2024Challenge.Backend.Modules.Classification.Application.IntegrationEvents.Handlers;
+
+public class MatchUpdatedHandler : IEventHandler<PlayersMatchBetsClaculated>
+{
+    public Task HandleAsync(PlayersMatchBetsClaculated integrationEvent, CancellationToken cancellationToken = default)
+    {
+        Console.WriteLine("PlayersMatchBetsClaculated integrationEvent");
+
+        return Task.CompletedTask;
+    }
+}
