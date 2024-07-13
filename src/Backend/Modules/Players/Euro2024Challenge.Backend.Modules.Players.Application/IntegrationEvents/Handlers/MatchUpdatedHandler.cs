e72c7db0b15cf50f390943cfd3ff435abadc1e84
krzpie <krzys_7@op.pl>
2024-07-13 08:07:01 +0200

    MatchUpdatedHandler

--- src/Backend/Modules/Players/Euro2024Challenge.Backend.Modules.Players.Application/IntegrationEvents/Handlers/MatchUpdatedHandler.cs
+++ src/Backend/Modules/Players/Euro2024Challenge.Backend.Modules.Players.Application/IntegrationEvents/Handlers/MatchUpdatedHandler.cs
@@ -1,14 +1,21 @@
-﻿using Euro2024Challenge.Backend.Modules.Tournament.Shared;
+﻿using Euro2024Challenge.Backend.Modules.Players.Shared.Events;
+using Euro2024Challenge.Backend.Modules.Tournament.Shared;
 using Euro2024Challenge.Shared;
 
-namespace Euro2024Challenge.Backend.Modules.Players.Application;
+namespace Euro2024Challenge.Backend.Modules.Players.Application.IntegrationEvents.Handlers;
 
 public class MatchUpdatedHandler : IEventHandler<MatchUpdated>
 {
-    public Task HandleAsync(MatchUpdated integrationEvent, CancellationToken cancellationToken = default)
+    private readonly IEventBus _eventBus;
+
+    public MatchUpdatedHandler(IEventBus eventBus)
+    {
+        _eventBus = eventBus;
+    }
+    public async Task HandleAsync(MatchUpdated integrationEvent, CancellationToken cancellationToken = default)
     {
         Console.WriteLine("MatchUpdatedHandler integrationEvent");
 
-        return Task.CompletedTask;
+        await _eventBus.PublishAsync(new PlayersMatchBetsClaculated());
     }
 }
