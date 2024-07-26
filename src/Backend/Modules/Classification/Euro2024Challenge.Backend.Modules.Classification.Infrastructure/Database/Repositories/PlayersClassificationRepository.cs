c91af701fa71f6eb6ff32a0f45cd9a7826bea148
krzpie <krzys_7@op.pl>
2024-07-26 21:40:19 +0200

    Apply mongo db model in classificatio repository

--- src/Backend/Modules/Classification/Euro2024Challenge.Backend.Modules.Classification.Infrastructure/Database/Repositories/PlayersClassificationRepository.cs
+++ src/Backend/Modules/Classification/Euro2024Challenge.Backend.Modules.Classification.Infrastructure/Database/Repositories/PlayersClassificationRepository.cs
@@ -1,5 +1,6 @@
 using Euro2024Challenge.Backend.Modules.Classification.Domain.Entities;
 using Euro2024Challenge.Backend.Modules.Classification.Domain.Repositories;
+using Euro2024Challenge.Backend.Modules.Classification.Infrastructure.Database.Models;
 using Euro2024Challenge.Backend.Modules.Classification.Infrastructure.Database.Settings;
 using Microsoft.Extensions.Options;
 using MongoDB.Driver;
@@ -8,13 +9,13 @@ namespace Euro2024Challenge.Backend.Modules.Classification.Infrastructure.Databa
 
 public class PlayersClassificationRepository : IClassificationRepository
 {
-    private readonly IMongoCollection<BetPoints> _playersClassificationCollection;
+    private readonly IMongoCollection<PlayerBetPoints> _playersClassificationCollection;
 
     public PlayersClassificationRepository(IOptions<ClassificationDatabaseSettings> options)
     {
         var mongoClient = new MongoClient(options.Value.ConnectionString);
         var mongoDatabase = mongoClient.GetDatabase(options.Value.DatabaseName);
-        _playersClassificationCollection = mongoDatabase.GetCollection<BetPoints>(options.Value.CollectionName);
+        _playersClassificationCollection = mongoDatabase.GetCollection<PlayerBetPoints>(options.Value.CollectionName);
     }
 
     public async Task<List<PlayersPoints>> GetAll()
@@ -24,14 +25,21 @@ public class PlayersClassificationRepository : IClassificationRepository
         return null;
     }
 
-    public async Task Insert(Guid PlayerId, BetPoints points)
+    public async Task Insert(Guid playerId, BetPoints points)
     {
-        await _playersClassificationCollection.InsertOneAsync(points);
+        PlayerBetPoints playerBetPoints = new()
+        {
+            PlayerId = playerId,
+            BetId = points.BetId,
+            Points = points.Points
+        };
+
+        await _playersClassificationCollection.InsertOneAsync(playerBetPoints);
     }
 
     public async Task<List<BetPoints>> Get(Guid playerId)
     {
-        var filter = Builders<BetPoints>.Filter.Eq("player_id", playerId);
+        var filter = Builders<PlayerBetPoints>.Filter.Eq("player_id", playerId);
         var result = await _playersClassificationCollection.Find(filter).FirstOrDefaultAsync();
         
         return [];
