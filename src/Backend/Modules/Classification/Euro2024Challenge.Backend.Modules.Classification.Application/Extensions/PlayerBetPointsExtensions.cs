50028728f5a649fd89f79ec12463913ca2a77e95
krzpie <krzys_7@op.pl>
2024-08-02 21:25:34 +0200

    Change extension class

--- src/Backend/Modules/Classification/Euro2024Challenge.Backend.Modules.Classification.Application/Extensions/PlayerBetPointsExtensions.cs
+++ src/Backend/Modules/Classification/Euro2024Challenge.Backend.Modules.Classification.Application/Extensions/PlayerBetPointsExtensions.cs
@@ -2,15 +2,15 @@
 
 namespace Euro2024Challenge.Backend.Modules.Classification.Application.Extensions;
 
-public static class BetPointsExtensions
+public static class PlayerBetPointsxtensions
 {
-    public static GetPlayerClassificationsResponse ToGetPlayerClassificationsResponse(this IEnumerable<Domain.Entities.BetPoints> betPoints, Guid playerId)
+    public static GetPlayerClassificationsResponse ToGetPlayerClassificationsResponse(this IEnumerable<Domain.Entities.PlayerBetPoints> betPoints, Guid playerId)
     {
         return new GetPlayerClassificationsResponse()
         {
             PlayerId = playerId,
             BetsPoints = betPoints
-                        .Select(x => new BetPointsDto() { BetId = x.BetId, Points = x.Points })
+                        .Select(x => new BetPointsDto() { BetId = Guid.Parse(x.BetId), Points = x.Points })
                         .ToList()
                         .AsReadOnly()
         };
