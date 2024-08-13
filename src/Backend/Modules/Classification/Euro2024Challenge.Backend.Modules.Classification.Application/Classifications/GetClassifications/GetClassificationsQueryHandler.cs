2080af64a1c11b26baed127a22e48be3ed462314
krzpie <krzys_7@op.pl>
2024-08-13 22:18:35 +0200

    call players module api

--- src/Backend/Modules/Classification/Euro2024Challenge.Backend.Modules.Classification.Application/Classifications/GetClassifications/GetClassificationsQueryHandler.cs
+++ src/Backend/Modules/Classification/Euro2024Challenge.Backend.Modules.Classification.Application/Classifications/GetClassifications/GetClassificationsQueryHandler.cs
@@ -1,6 +1,7 @@
 ﻿using Euro2024Challenge.Backend.Modules.Classification.Application.Extensions;
 using Euro2024Challenge.Backend.Modules.Classification.Domain.Entities;
 using Euro2024Challenge.Backend.Modules.Classification.Domain.Repositories;
+using Euro2024Challenge.Backend.Modules.Players.Shared;
 using MediatR;
 
 namespace Euro2024Challenge.Backend.Modules.Classification.Application.Classifications.GetClassifications;
@@ -8,15 +9,19 @@ namespace Euro2024Challenge.Backend.Modules.Classification.Application.Classific
 public class GetClassificationsQueryHandler : IRequestHandler<GetClassificationsQuery, IReadOnlyCollection<GetClassificationsResponse>>
 {
     private readonly IClassificationRepository _classificationRepository;
+    private readonly IPlayerModuleApi _playerModuleApi;
 
-    public GetClassificationsQueryHandler(IClassificationRepository classificationRepository)
+    public GetClassificationsQueryHandler(IClassificationRepository classificationRepository, IPlayerModuleApi playerModuleApi)
     {
         _classificationRepository = classificationRepository;
+        _playerModuleApi = playerModuleApi;
     }
 
     public async Task<IReadOnlyCollection<GetClassificationsResponse>> Handle(GetClassificationsQuery request, CancellationToken cancellationToken)
     {
         IReadOnlyCollection<PlayerBetPoints> betsPoints = await _classificationRepository.GetAll();
+
+        await _playerModuleApi.GetPlayersUsernames(betsPoints.Select(x => Guid.Parse(x.PlayerId)));
 
         return betsPoints.ToGetClassificationsResponse().AsReadOnly();
     }
