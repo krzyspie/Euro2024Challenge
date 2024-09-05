6bf6187dcb24a8313878138a212095c6beba6bcb
krzpie <krzys_7@op.pl>
2024-09-05 21:43:09 +0200

    GetByNumbers test

--- tests/Modules/Tournaments/Tournamnets.Core.Tests/Services/MatchServiceTests.cs
+++ tests/Modules/Tournaments/Tournamnets.Core.Tests/Services/MatchServiceTests.cs
@@ -146,4 +146,81 @@ public class MatchServiceTests
             Assert.That(result.StartHour, Is.EqualTo(matchDate));
         });
     }
+
+    [Test]
+    public async Task GetByNumbersReturnsAMatchesForGivenNumbers()
+    {
+        //Arrange
+        var matchDate = DateTime.Today;
+        var matches = new List<Match>()
+        {
+            new() {
+                Id = 1,
+                Number = 1,
+                AwayTeamId = 111,
+                AwayTeamGoals = 0,
+                GuestTeamId = 112,
+                GuestTeamGoals = 1,
+                StartHour = matchDate
+            },
+            new() {
+                Id = 2,
+                Number = 2,
+                AwayTeamId = 113,
+                AwayTeamGoals = 2,
+                GuestTeamId = 114,
+                GuestTeamGoals = 2,
+                StartHour = matchDate
+            }
+        };
+
+        var teams = new Dictionary<int, string>()
+        {
+            { 111, "Albania" },
+            { 112, "Belgium" },
+            { 113, "Croatia" },
+            { 114, "Portugal" },
+        };
+        
+        int[] numbers = [1, 2];
+        _matchRepositorySubstitute.GetByNumbers(numbers).Returns(matches);
+        _teamRepositorSubstitute.GetAllTeamsAsync().Returns(teams);
+
+        matchService = new MatchService(_matchRepositorySubstitute, _teamRepositorSubstitute, _eventBusSubstitute);
+
+        //Act
+        var result = await matchService.GetByNumbers(numbers);
+
+        //Assert
+        Assert.That(result, Is.Not.Empty);
+        Assert.That(result, Has.Count.EqualTo(2));
+        
+        var first = result.First();
+        Assert.Multiple(() =>
+        {
+            Assert.That(first.Id, Is.EqualTo(1));
+            Assert.That(first.Number, Is.EqualTo(1));
+            Assert.That(first.GuestTeamId, Is.EqualTo(112));
+            Assert.That(first.GuestTeamName, Is.EqualTo("Belgium"));
+            Assert.That(first.GuestTeamGoals, Is.EqualTo(1));
+            Assert.That(first.AwayTeamId, Is.EqualTo(111));
+            Assert.That(first.AwayTeamName, Is.EqualTo("Albania"));
+            Assert.That(first.AwayTeamGoals, Is.EqualTo(0));
+            Assert.That(first.StartHour, Is.EqualTo(matchDate));
+        });
+
+        var second = result.Last();
+        Assert.Multiple(() =>
+        {
+            Assert.That(second.Id, Is.EqualTo(2));
+            Assert.That(second.Number, Is.EqualTo(2));
+            Assert.That(second.GuestTeamId, Is.EqualTo(114));
+            Assert.That(second.GuestTeamName, Is.EqualTo("Portugal"));
+            Assert.That(second.GuestTeamGoals, Is.EqualTo(2));
+            Assert.That(second.AwayTeamId, Is.EqualTo(113));
+            Assert.That(second.AwayTeamName, Is.EqualTo("Croatia"));
+            Assert.That(second.AwayTeamGoals, Is.EqualTo(2));
+            Assert.That(second.StartHour, Is.EqualTo(matchDate));
+        });
+    }
 }
