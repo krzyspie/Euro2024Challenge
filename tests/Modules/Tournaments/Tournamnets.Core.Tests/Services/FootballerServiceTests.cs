af86191eff125387d0c1cfd228410c2206b009cd
krzpie <krzys_7@op.pl>
2024-09-08 21:54:20 +0200

    Add FootballerService test class

---
+++ tests/Modules/Tournaments/Tournamnets.Core.Tests/Services/FootballerServiceTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Modules.Tournamnets.Core.Tests.Services
+{
+    public class FootballerServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
