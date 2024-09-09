b965f49bfb49635b0fe08e0c7d350993d3ea7ba9
krzpie <krzys_7@op.pl>
2024-09-09 20:45:40 +0200

    Get footballer test

--- tests/Modules/Tournaments/Tournamnets.Core.Tests/Services/FootballerServiceTests.cs
+++ tests/Modules/Tournaments/Tournamnets.Core.Tests/Services/FootballerServiceTests.cs
@@ -1,4 +1,6 @@
+using Euro2024Challenge.Backend.Modules.Tournaments.Core.Entities;
 using Euro2024Challenge.Backend.Modules.Tournaments.Core.Repositories;
+using Euro2024Challenge.Backend.Modules.Tournaments.Core.Services;
 using NSubstitute;
 
 namespace Modules.Tournamnets.Core.Tests.Services
@@ -6,6 +8,7 @@ namespace Modules.Tournamnets.Core.Tests.Services
     public class FootballerServiceTests
     {
         private IFootballerRepository _footballerRepositorySubstitute;
+        private FootballerService _footballerService;
 
         [SetUp]
         public void Setup()
@@ -14,9 +17,37 @@ namespace Modules.Tournamnets.Core.Tests.Services
         }
 
         [Test]
-        public void Test1()
+        public async Task GetReturnsFootballerResponse()
         {
-            Assert.Pass();
+            //Arrange
+            int id = 1;
+            Footballer footballer = new()
+            {
+                Id = 1,
+                FullName = "Test Name",
+                Goals = 7,
+                TeamId = 99,
+                Team = new()
+                {
+                    Name = "Poland"
+                }
+            };
+
+            _footballerRepositorySubstitute.Get(id).Returns(footballer);
+            
+            _footballerService = new FootballerService(_footballerRepositorySubstitute);
+
+            //Act
+            var result = await _footballerService.Get(id);
+
+            //Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(result.FullName, Is.EqualTo(footballer.FullName));
+                Assert.That(result.Goals, Is.EqualTo(footballer.Goals));
+                Assert.That(result.Id, Is.EqualTo(footballer.Id));
+                Assert.That(result.TeamName, Is.EqualTo(footballer.Team.Name));
+            });
         }
     }
 }
