2d4ce1ded77d280c7b16f46c41e70a2e580f7faf
krzpie <krzys_7@op.pl>
2024-09-12 20:40:49 +0200

    GetTeams tests

--- tests/Modules/Tournaments/Tournamnets.Core.Tests/Services/TeamServiceTests.cs
+++ tests/Modules/Tournaments/Tournamnets.Core.Tests/Services/TeamServiceTests.cs
@@ -70,5 +70,66 @@ namespace Modules.Tournamnets.Core.Tests.Services
             await _teamRepositorSubstitute.DidNotReceive().GetAllTeamsAsync();
             _teamCacheSubstitute.DidNotReceive().Set(Arg.Any<Dictionary<int, string>>());
         }
+
+        [Test]
+        public async Task GetTeamsReturnsTeamsWithGivenIds()
+        {
+            //Arrange
+            int teamId = 2;
+            int teamId2 = 3;
+            string teamName = "Germany";
+            string teamName2 = "France";
+            Dictionary<int, string> allTeams = new (){{ 1, "Poland" }, { teamId, teamName }, { teamId2, teamName2 }};
+
+            _teamCacheSubstitute.TryGetValue(out Arg.Any<Dictionary<int, string>?>()).Returns(false);
+            _teamRepositorSubstitute.GetAllTeamsAsync().Returns(allTeams);
+
+            _teamService = new TeamService(_teamRepositorSubstitute, _teamCacheSubstitute);
+            
+            //Act
+            var result = await _teamService.GetTeamsAsync([teamId, teamId2]);
+
+            //Assert
+            Assert.That(result, Is.Not.Empty);
+            Assert.That(result.Count(), Is.EqualTo(2));
+            var item1 = result.Single(x => x.Id == teamId);
+            Assert.That(item1.Name, Is.EqualTo(teamName));
+            var item2 = result.Single(x => x.Id == teamId2);
+            Assert.That(item2.Name, Is.EqualTo(teamName2));
+
+            _teamCacheSubstitute.Received(1).Set(allTeams);
+        }
+
+        [Test]
+        public async Task GetTeamsReturnsTeamsWithGivenIdsFromCache()
+        {
+            //Arrange
+            int teamId = 2;
+            int teamId2 = 3;
+            string teamName = "Germany";
+            string teamName2 = "France";
+            Dictionary<int, string> allTeams = new (){{ 1, "Poland" }, { teamId, teamName }, { teamId2, teamName2 }};
+
+            _teamCacheSubstitute.TryGetValue(out Arg.Any<Dictionary<int, string>?>()).Returns(x => {
+                x[0] = allTeams;
+                return true;
+                });
+
+            _teamService = new TeamService(_teamRepositorSubstitute, _teamCacheSubstitute);
+            
+            //Act
+            var result = await _teamService.GetTeamsAsync([teamId, teamId2]);
+
+            //Assert
+            Assert.That(result, Is.Not.Empty);
+            Assert.That(result.Count(), Is.EqualTo(2));
+            var item1 = result.Single(x => x.Id == teamId);
+            Assert.That(item1.Name, Is.EqualTo(teamName));
+            var item2 = result.Single(x => x.Id == teamId2);
+            Assert.That(item2.Name, Is.EqualTo(teamName2));
+
+            await _teamRepositorSubstitute.DidNotReceive().GetAllTeamsAsync();
+            _teamCacheSubstitute.DidNotReceive().Set(Arg.Any<Dictionary<int, string>>());
+        }
     }
 }
