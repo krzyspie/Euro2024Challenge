d890de917a4cfec2e3ce3e46e46853c626799206
krzpie <krzys_7@op.pl>
2024-09-22 16:15:26 +0200

    GetPlayersQueryHandler implementation

--- src/Backend/Modules/Players/Euro2024Challenge.Backend.Modules.Players.Application/Players/Get/GetPlayersQueryHandler.cs
+++ src/Backend/Modules/Players/Euro2024Challenge.Backend.Modules.Players.Application/Players/Get/GetPlayersQueryHandler.cs
@@ -1,13 +1,21 @@
 
 using Euro2024Challenge.Backend.Modules.Players.Application.Players.DTO;
+using Euro2024Challenge.Backend.Modules.Players.Domain.Repositories;
 using MediatR;
 
 namespace Euro2024Challenge.Backend.Modules.Players.Application.Players.Get;
 
 public class GetPlayersQueryHandler : IRequestHandler<GetPlayersQuery, IEnumerable<PlayerDto>>
 {
-    public Task<IEnumerable<PlayerDto>> Handle(GetPlayersQuery request, CancellationToken cancellationToken)
+    private readonly IPlayersRepository _playersRepository;
+
+    public GetPlayersQueryHandler(IPlayersRepository playersRepository)
     {
-        throw new NotImplementedException();
+        _playersRepository = playersRepository;
+    }
+
+    public async Task<IEnumerable<PlayerDto>> Handle(GetPlayersQuery request, CancellationToken cancellationToken)
+    {
+        return await _playersRepository.GetAllPlayers();
     }
 }
