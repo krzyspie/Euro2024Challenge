43ac99a0c7bee2748ede990634df3d782a9ad6d8
krzpie <krzys_7@op.pl>
2024-10-04 21:19:46 +0200

    basic display players

--- src/Frontend/Euro2024Challange/Program.cs
+++ src/Frontend/Euro2024Challange/Program.cs
@@ -6,6 +6,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddScoped(sp => 
+    new HttpClient
+    {
+        BaseAddress = new Uri("http://localhost:5047")
+    });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
