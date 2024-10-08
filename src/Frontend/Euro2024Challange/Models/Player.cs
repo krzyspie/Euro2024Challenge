efe82e40a8b19d43700506bbc5b346c32330a37c
krzpie <krzys_7@op.pl>
2024-10-08 22:27:29 +0200

    Move models to separate folder

---
+++ src/Frontend/Euro2024Challange/Models/Player.cs
@@ -0,0 +1,7 @@
+namespace Euro2024Challange.Models;
+
+public class Player
+{
+    public string Email {get; set; }
+    public string Username {get; set; }
+}
---
+++ src/Frontend/Euro2024Challange/Models/PlayersClassification.cs
@@ -0,0 +1,7 @@
+namespace Euro2024Challange.Models;
+
+public class PlayersClassification
+{
+    public string PlayerUsername { get; set; }
+    public int Points { get; set; }
+}
